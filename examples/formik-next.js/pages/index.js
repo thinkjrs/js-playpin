ea65de991797fff7e10f62da152011d06dc5ce36
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-04 11:30:08 -0500

    add initial formik tutorial boilerplate

---
+++ examples/formik-next.js/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+
+function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Formik-Next.js Tutorial</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>A <code>Formik</code> & <code>Next.js</code> exemplar</h1>
+      <p>Hello, world!</p>
+    </div>
+  );
+};
+
+export default Index;

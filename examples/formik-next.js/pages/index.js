7ee15c5b7418a67017613b14306368a7d5734e16
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-04 13:49:01 -0500

    cleanup demo form markup

--- examples/formik-next.js/pages/index.js
+++ examples/formik-next.js/pages/index.js
@@ -9,8 +9,13 @@ function Index() {
         <title>Formik-Next.js Tutorial</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>A <code>Formik</code> & <code>Next.js</code> exemplar</h1>
-      <p>Hello, world!</p>
+      <h3>A <code>Formik</code> <code>Next.js</code> exemplar</h3>
+      <blockquote>
+        <em>Forms with 
+          &nbsp;<code>Formik</code> and <code>Next.js</code> 
+          &nbsp;are awesome!
+        </em>
+      </blockquote>
       <SignupForm />
     </div>
   );

3e983588bc477081b8e92763ac2bdf85d8de51a6
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-04 15:10:12 -0500

    use formik Form, Field, and ErrorMessage components

--- examples/formik-next.js/pages/signup.js
+++ examples/formik-next.js/pages/signup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 
@@ -7,51 +7,48 @@ import * as Yup from 'yup';
 const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: ''
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(25, 'Must be 25 characters or less')
-        .required('Required'),
-      lastName: Yup.string()
-        .max(25, 'Must be 25 characters or less')
-        .required('Required'),
-      email: Yup.string()
-        .max(80, 'Must be 80 characters or less')
-        .email('Invalid email address')
-        .required('Required'),
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor="firstName">First name</label>
-        <input name="firstName" type="text" {...formik.getFieldProps('firstName')} />
-        {formik.touched.firstName && formik.errors.firstName ? (
-          <div>{formik.errors.firstName}</div>
-         ) : null}
-        <label htmlFor="lastName">Last name</label>
-        <input name="lastName" type="text" {...formik.getFieldProps('lastName')} />
-        {formik.touched.lastName && formik.errors.lastName ? (
-          <div>{formik.errors.lastName}</div>
-         ) : null}
-        <label htmlFor="email">Email address</label>
-        <input name="email" type="email" {...formik.getFieldProps('email')} />
-        {formik.touched.email && formik.errors.email ? (
-          <div>{formik.errors.email}</div>
-         ) : null}
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <Formik
+      initialValues={{ firstName: '', lastName: '', email: '' }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(25, 'Must be 25 characters or less')
+          .required('Required'),
+        lastName: Yup.string()
+          .max(25, 'Must be 25 characters or less')
+          .required('Required'),
+        email: Yup.string()
+          .max(80, 'Must be 80 characters or less')
+          .email('Invalid email address')
+          .required('Required'),
+        })
+      }
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {formik => (
+        <Form>
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" type="text" />
+            <ErrorMessage name="firstName" />
+            <label htmlFor="lastName">First Name</label>
+            <Field name="lastName" type="text" />
+            <ErrorMessage name="lastName" />
+            <label htmlFor="email">Email Address</label>
+            <Field name="email" type="text" />
+            <ErrorMessage name="email" />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 

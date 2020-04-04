042fb13438d1dfa016fb33babe7d53e8b8c39390
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-04 12:16:01 -0500

    add firstName and lastName form fields

--- examples/formik-next.js/pages/signup.js
+++ examples/formik-next.js/pages/signup.js
@@ -15,11 +15,30 @@ const SignupForm = () => {
   });
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
+      <label htmlFor="firstName">First name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        placeholder="Enter your first name"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      <label htmlFor="lastName">Last name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        placeholder="Enter your last name"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+      <label htmlFor="email">Email address</label>
       <input
         id="email"
         name="email"
         type="email"
+        placeholder="Enter your email"
         onChange={formik.handleChange}
         value={formik.values.email}
       />

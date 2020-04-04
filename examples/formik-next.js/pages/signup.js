044f53409dae7a17332847bf1615b5ee361dcf01
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-04 12:36:25 -0500

    add better validation w/'touched/blur'

--- examples/formik-next.js/pages/signup.js
+++ examples/formik-next.js/pages/signup.js
@@ -2,6 +2,34 @@ import React from 'react';
 import { useFormik } from 'formik';
 
 
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 50) {
+    errors.firstName = 'Must be 50 characters or less';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 50) {
+    errors.lastName = 'Must be 50 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  } else if (values.email.length > 100) {
+    errors.email = 'Email address too long, must be less than 100 characters';
+  }
+  return errors;
+};
+
+
+// A boilerplate, custom signup with Formik
 const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
@@ -9,6 +37,7 @@ const SignupForm = () => {
     initialValues: {
       email: '',
     },
+    validate,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -22,8 +51,12 @@ const SignupForm = () => {
         type="text"
         placeholder="Enter your first name"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.firstName}
       />
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+       ) : null}
       <label htmlFor="lastName">Last name</label>
       <input
         id="lastName"
@@ -31,8 +64,12 @@ const SignupForm = () => {
         type="text"
         placeholder="Enter your last name"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.lastName}
       />
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+       ) : null}
       <label htmlFor="email">Email address</label>
       <input
         id="email"
@@ -40,8 +77,12 @@ const SignupForm = () => {
         type="email"
         placeholder="Enter your email"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
       />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+       ) : null}
       <button type="submit">Submit</button>
     </form>
   );

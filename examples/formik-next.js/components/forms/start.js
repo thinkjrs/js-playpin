0efc20bb99b8918d42a56e687518ec6a436f3eb7
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-05 17:43:38 -0500

    remove superfluous component imports

--- examples/formik-next.js/components/forms/start.js
+++ examples/formik-next.js/components/forms/start.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
 import { Field, ErrorMessage } from 'formik';
-import setImmediate from 'setimmediate';
 
 
 // A boilerplate, custom signup with Formik
 function StartInfo() {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
-  var _setImmediate = setImmediate;
-  process.once('loaded', function() {
-    global.setImmediate = _setImmediate;
-  });
+
   return (
     <div>
       <label htmlFor="email" />

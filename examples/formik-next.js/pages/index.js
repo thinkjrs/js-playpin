4dd6a90d8d61fac20ca28e24ec66f5117312e82d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-06 17:11:28 -0500

    update header and blockquote styling

--- examples/formik-next.js/pages/index.js
+++ examples/formik-next.js/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import {FormikWizard} from 'formik-wizard';
 import steps from '../components/steps';
-import { useCallback } from 'react';
 import setImmediate from 'setimmediate';
 import FormWrapper from '../components/FormWrapper';
 
@@ -11,24 +10,17 @@ function OnboardingApp() {
   process.once('loaded', function() {
     global.setImmediate = _setImmediate;
   });
-  const handleSubmit = useCallback((values) => {
-    console.log('full values:', values)
-
-    return {
-      message: 'Thanks for submitting!',
-    }
-  }, [])
   return (
-    <div className="container mx-auto px-4">
+    <div className="container px-3">
       <Head>
         <title>Formik-Next.js Tutorial</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <>
-        <h3>A <code>Formik</code> <code>Next.js</code> exemplar</h3>
-        <blockquote>
+        <h3 className="text-lg text-bold">A <code>Formik</code> <code>Next.js</code> exemplar</h3>
+        <blockquote className="bg-gray-300">
           <em>Forms with 
-            &nbsp;<code>Formik</code> and <code>Next.js</code> 
+            &nbsp;<code className="border-left text-xs">Formik</code> and <code>Next.js</code> 
             &nbsp;are awesome!
           </em>
         </blockquote>

5bf0030ac557c67ea72acd1c61168667fb3c4804
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-05 22:39:46 -0500

    fix classes to naively utilize tailwindcss

--- examples/formik-next.js/components/forms/digital.js
+++ examples/formik-next.js/components/forms/digital.js
@@ -5,13 +5,15 @@ import { Field, ErrorMessage } from 'formik';
 // A boilerplate, custom signup with Formik
 function DigitalInfo() {
   return (
-    <div> 
-      <label htmlFor="monthlyListenersSpotify" />
-      <Field name="monthlyListenersSpotify" placeholder="Last month's 'monthly' listeners on Spotify" type="text" />
-      <ErrorMessage name="monthlyListenersSpotify" />
-      <label htmlFor="youtubeUrl" />
-      <Field name="youtubeUrl" placeholder="Link to your best video" type="text" />
-      <ErrorMessage name="youtubeUrl" />
+    <div className="flex flex-col mb-4"> 
+      <div className="col-span-3 py-2">
+        <Field name="monthlyListenersSpotify" className="border py-1 px-3 text-grey-darkest bg-gray-200 hover:bg-white" placeholder="Last month's 'monthly' listeners on Spotify" type="text" />
+        <ErrorMessage name="monthlyListenersSpotify" />
+      </div>
+      <div className="col-span-3 py-2">
+        <Field name="youtubeUrl" className="border py-1 px-3 text-grey-darkest bg-gray-200 hover:bg-white" placeholder="Link to your best video" type="text" />
+        <ErrorMessage name="youtubeUrl" />
+      </div>
     </div>
   );
 };
--- examples/formik-next.js/components/forms/you.js
+++ examples/formik-next.js/components/forms/you.js
@@ -5,16 +5,19 @@ import { Field, ErrorMessage } from 'formik';
 // A boilerplate, custom signup with Formik
 function YouInfo() {
   return (
-    <div>
-      <label htmlFor="artistName" />
-      <Field name="artistName" placeholder="Artist/Band name" type="text" />
-      <ErrorMessage name="artistName" />
-      <label htmlFor="firstName" />
-      <Field name="firstName" placeholder="Your first name" type="text" />
-      <ErrorMessage name="firstName" />
-      <label htmlFor="lastName" />
-      <Field name="lastName" placeholder="Your last name" type="text" />
-      <ErrorMessage name="lastName" />
+    <div className="flex flex-col mb-4">
+      <div className="col-span-3 py-2">
+        <Field name="artistName" className="border bg-gray-200 px-3 py-1 text-grey-darkest" placeholder="Artist/Band name" type="text" />
+        <ErrorMessage name="artistName" />
+      </div>
+      <div class="col-span-3 py-2">
+        <Field name="firstName" className="border bg-gray-200 px-3 py-1 text-grey-darkest" placeholder="Your first name" type="text" />
+        <ErrorMessage name="firstName" />
+      </div>
+      <div class="col-span-3 py-2">
+        <Field name="lastName" className="border bg-gray-200 px-3 py-1 text-grey-darkest" placeholder="Your last name" type="text" />
+        <ErrorMessage name="lastName" />
+      </div>
     </div>
   );
 };

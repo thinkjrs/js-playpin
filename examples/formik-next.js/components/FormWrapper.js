9946ae8a7c40ce42ea61e291fb448597f958f0e0
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-06 17:11:12 -0500

    update button styling

--- examples/formik-next.js/components/FormWrapper.js
+++ examples/formik-next.js/components/FormWrapper.js
@@ -7,9 +7,9 @@ function FormWrapper({
     actionLabel,
   }) {
     let disabled = !canGoBack ? "cursor-not-allowed bg-gray-200" : "font-bold bg-gray-300 hover:bg-gray-400"
-    let btnClassName = disabled + " text-gray-800 py-2 px-4 rounded-l"
+    let btnClassName = disabled + " text-gray-800 w-40 rounded-l py-2"
     return (
-      <div className="container mx-auto">
+      <div>
         {status && (
           <div>
             {status.message}
@@ -20,8 +20,8 @@ function FormWrapper({
           <button className={btnClassName}  type="button" onClick={goToPreviousStep} disabled={!canGoBack}>
             Previous
           </button>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r" type="submit">
-            {actionLabel || (isLastStep ? 'Submit' : 'Next step')}
+          <button className="bg-gray-300 hover:bg-gray-400 w-40 text-gray-800 font-bold py-2 rounded-r" type="submit">
+            {actionLabel || (isLastStep ? 'Submit ' : 'Next')}
           </button>
         </div>
       </div>

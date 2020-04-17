31c78bbe82215fd7b483ae6841f11c5d9391a282
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-17 11:13:30 -0500

    :recycle: add in fan-booster elements

--- examples/bridge-sanity-fan-booster/pages/index.js
+++ examples/bridge-sanity-fan-booster/pages/index.js
@@ -1,20 +1,53 @@
-import Container from '../components/container'
-import Layout from '../components/layout'
-import { getFanBoosterThoughts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import { getFanBoosterThoughts } from '../lib/api'
+import { BIZ_NAME, PROD_NAME } from '../lib/constants'
+import Layout from '../components/layout'
+import Navbar from '../components/navbar';
+import { Spend } from '../components/spend';
+import { Status } from '../components/status';
+import { Objectives } from '../components/objectives';
+import { Done, Doing } from '../components/details';
+import Thoughts from '../components/thoughts';
+import React from "react";
+import {
+    Text,
+    Box,
+} from "@chakra-ui/core";
+
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { Footer } from "../components/Footer";
+
 
 export default function Index({ thoughts, preview }) {
+  
   return (
-    <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>{PROD_NAME} by {BIZ_NAME}</title>
         </Head>
+        <Navbar />
         <Container>
+          <Main>
+            <Box pt={50} />
+            <Hero />
+            <Spend />
+            <Status />
+            <Objectives />
+            <Doing />
+            <Done />
+            {thoughts && (
+              <Thoughts contents={thoughts}/>
+            )}
+          <Footer>
+            <Text textAlign="left">Made with ❤️ for artists.</Text>
+          </Footer>
+          </Main>
+          <DarkModeSwitch />
         </Container>
       </Layout>
-    </>
   )
 }
 

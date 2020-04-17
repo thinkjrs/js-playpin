27c041a70a4e59cc1c85be7add33fc50af756080
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-16 23:59:52 -0500

    :sparkles: :tada: add initial bridge application struction

---
+++ examples/bridge-sanity-fan-booster/pages/index.js
@@ -0,0 +1,46 @@
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPostsForHome, getFanBoosterThoughts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+
+export default function Index({ thoughts, allPosts, preview }) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          <h3>{thoughts[0].section_title}</h3>
+          <p>{thoughts[0].section_content}</p>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export async function getStaticProps({ preview = false }) {
+  const allPosts = await getAllPostsForHome(preview)
+  const thoughts = await getFanBoosterThoughts()
+  return {
+    props: { thoughts, allPosts, preview },
+  }
+}

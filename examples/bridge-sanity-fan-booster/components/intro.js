d64f5bf2e603a265ad3b922c65db773c1cc15a44
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-17 16:49:36 -0500

    :sparkles: add basic overrides

---
+++ examples/bridge-sanity-fan-booster/components/intro.js
@@ -0,0 +1,22 @@
+import { BIZ_URL, PROD_URL, BIZ_NAME, PROD_NAME } from '../lib/constants'
+
+export default function Intro() {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        Fan.Booster
+      </h1>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+        Your bespoke tactical digital marketing solution from{' '}
+        <a
+          href={BIZ_URL}
+          className="underline hover:text-success duration-200 transition-colors"
+        >
+          {BIZ_NAME} 
+        </a>
+        .
+      </h4>
+    </section>
+  )
+}
+ 

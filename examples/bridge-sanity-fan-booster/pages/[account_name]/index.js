cf19f83a211d8d1e349314ccf9cb7f342c9d1efd
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-04-21 21:42:31 -0500

    :construction: add getStaticPaths method

--- examples/bridge-sanity-fan-booster/pages/[account_name]/index.js
+++ examples/bridge-sanity-fan-booster/pages/[account_name]/index.js
@@ -3,10 +3,9 @@ import MoreStories from '../../components/more-stories'
 import HeroPost from '../../components/hero-post'
 import Intro from '../../components/intro'
 import Layout from '../../components/layout'
-import { getAllPostsForHome } from '../../lib/api'
+import { getAccountName, getAllPostsForHome } from '../../lib/api'
 import Head from 'next/head'
 import { BIZ_NAME, PROD_NAME} from '../../lib/constants'
-
 export default function Index({ allPosts, preview }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
@@ -37,7 +36,21 @@ export default function Index({ allPosts, preview }) {
 
 export async function getStaticProps({ preview = false }) {
   const allPosts = await getAllPostsForHome(preview)
+  const account = await getAccountName()
   return {
-    props: { allPosts, preview },
+    props: { account, allPosts, preview },
+  }
+}
+
+export async function getStaticPaths() {
+  const account = await getAccountName()
+  console.log(account[0])
+  return {
+    paths: [
+      { params: {
+        account_name: 'test',//account[0].account_name
+      }}, 
+    ],
+    fallback: true
   }
 }

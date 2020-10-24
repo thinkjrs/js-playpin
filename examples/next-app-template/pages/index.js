7cb7395079dbd26052a2605c78c6c61d458840de
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-10-23 23:12:35 -0500

    :wrench: :construction: wip commit adds login and signup endpoints

--- examples/next-app-template/pages/index.js
+++ examples/next-app-template/pages/index.js
@@ -1,52 +1,36 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Musicfox Sing Up</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://musicfox.io">Musicfox.</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Bringing democracy to music{' '}
+          <br></br>
+          <code className={styles.code}>at least you get it somewhere</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Sign Up&rarr;</h3>
+            <p>Sign Up for a Musicfox account. <strong>It's free!</strong></p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Login&rarr;</h3>
+            <p>Login to your Musicfox account. Access your b00st apps and projects.</p>
           </a>
         </div>
       </main>

cdc6fafe185589ce19db749d04a48cf68a6cb54d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-10-28 16:29:22 -0500

    :recycle: basic button replacement and jsx styling

--- examples/next-app-template/pages/signup.tsx
+++ examples/next-app-template/pages/signup.tsx
@@ -43,14 +43,14 @@ function error404(): JSX.Element {
 };
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-function FormOptions({checkboxContent}) {
+function FormOptions({checkboxContent, step}) {
   return (
     <>
       <div id="checkbox-group" />
       <div role="group" aria-labelledby="checkbox-group">
-        { checkboxContent.map((val, index) => (
+        { checkboxContent.map((val) => (
           <label>
-            <Field type="checkbox" name={`${checkboxContent[index]}`} value={checkboxContent[index]} />
+            <Field type="checkbox" name={`${step}.${val}`} value={val}/>
             {val}
           </label>
           )) 
@@ -60,21 +60,33 @@ function FormOptions({checkboxContent}) {
   )
 };
 function Basic () {
+  const buttonStyles = `
+    background-color: #c60000;
+    color: inherit;
+    text-align: inherit;
+    //box-sizing: inherit;
+    margin: 16;
+    padding: 1;
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    text-align: center;
+  `;
   const defaultFormRes = {
     0: ['This', 'Is', 'A', 'Test'],
     1: ['Another','Test', 'Here'],
     2: ["Don't", 'Mind', 'Me'],
     3: ['Unless', 'The', 'Test', 'Fails']
   };
+  const steps = 4; 
   const [step, setStep] = useState(0);
   const [formRes, setFormRes] = useState( defaultFormRes );
   return (
     <div>
       <h1>Sign Up</h1>
       <Formik
-        initialValues={{
-          checked: [], /* name of Field "chedked" */
-        }}
+        initialValues={
+          Object.entries(defaultFormRes).map((value, index) => {value: index})
+        }
         onSubmit={async (values) => {
           await sleep(150);
           alert(JSON.stringify(values, null, 2));
@@ -82,16 +94,21 @@ function Basic () {
       >
         {({ values }) => (
           <Form>              
-            <FormOptions  checkboxContent={defaultFormRes[step]}/>
+            <FormOptions checkboxContent={defaultFormRes[step]}/>
             <div style={{display: "flex"}}>
-              <div className={styles.card} style={{backgroundColor: "#999193", color: '#fff'}} onClick={() => setStep(validateStep(step - 1))}>
-                Regress.
-              </div>
+              <button className={styles.card} type="button" style={{backgroundColor: "#999193", color: '#fff'}} onClick={() => setStep(validateStep(step - 1))}>
+                Back
+                <style jsx>{buttonStyles}</style>
+              </button>
               { step == 3 
-                ? <button className={styles.card} style={{backgroundColor: "#c60000", color: '#fff'}} type="submit">Submit</button> 
-                : <div className={styles.card} style={{backgroundColor: "#777", color: '#fff'}}  onClick={() => setStep(validateStep(step+1))}>
-                  Progress.
-                </div>
+                ? <button className={styles.card} style={{backgroundColor: "#c60000", color: '#fff'}} type="submit">
+                    Submit
+                    <style jsx>{buttonStyles}</style>
+                  </button> 
+                : <button className={styles.card} style={{backgroundColor: "#777", color: '#fff'}}  type="button" onClick={() => setStep(validateStep(step+1))}>
+                    Next 
+                    <style jsx>{buttonStyles}</style>
+                </button>
               }
             </div>
             
